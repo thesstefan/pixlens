4701db27835b1aaa428ce188115efc18b65fce57
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-11-27 20:58:40 +0100

    Add initial setup for the pixlens package

---
+++ pixlens/cli/pixlens_cli.py
@@ -0,0 +1,15 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="PixLens - Evaluate & understand image editing models"
+)
+
+
+def main() -> None:
+    parser.parse_args()
+
+    print("PixLens - Hello!")
+
+
+if __name__ == "__main__":
+    main()
