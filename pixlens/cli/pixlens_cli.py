3a9293654c10583019120a4a90cdc8adc2592aba
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-12-11 22:53:45 +0100

    Add visualization package and basic GroundingSAM inference example

--- pixlens/cli/pixlens_cli.py
+++ pixlens/cli/pixlens_cli.py
@@ -1,13 +1,17 @@
 import argparse
 
+import numpy as np
 import torch
+from PIL import Image
 
 from pixlens.eval import grounded_sam
+from pixlens.visualization import annotation
 
 parser = argparse.ArgumentParser(
     description="PixLens - Evaluate & understand image editing models"
 )
 
+parser.add_argument("--out", type=str, help=("Path of output annotated image"))
 parser.add_argument("--image", type=str, help=("Image to detect objects in"))
 parser.add_argument("--prompt", type=str, help=("Prompt to guide detection"))
 
@@ -20,6 +24,16 @@ def main() -> None:
     segmentation_output, detection_output = model.detect_and_segment(
         args.prompt, args.image
     )
+
+    image_source = np.asarray(Image.open(args.image).convert("RGB"))
+
+    annotated_image = annotation.annotate_detection_output(
+        image_source, detection_output
+    )
+    masked_annotated_image = annotation.show_mask(
+        segmentation_output.masks[0][0], annotated_image
+    )
+    masked_annotated_image.save(args.out)
 
 
 if __name__ == "__main__":
---
+++ pixlens/visualization/annotation.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+from groundingdino.util import box_ops, inference
+from PIL import Image
+
+from pixlens.eval import interfaces
+
+
+# TODO: Decouple this from GroundingDINO, it should work for any bounding boxes.
+def annotate_detection_output(
+    image_source: np.ndarray, detection_output: interfaces.DetectionOutput
+) -> Image.Image:
+    height, width, _ = image_source.shape
+    bbox = detection_output.bounding_boxes / torch.Tensor(
+        [width, height, width, height]
+    )
+    bbox_cxcywh = box_ops.box_xyxy_to_cxcywh(bbox)
+
+    annotated_frame = inference.annotate(
+        image_source=image_source,
+        boxes=bbox_cxcywh,
+        logits=detection_output.logits,
+        phrases=detection_output.phrases,
+    )[..., ::-1]
+
+    return Image.fromarray(annotated_frame)
+
+
+def show_mask(mask: torch.Tensor, image: Image.Image):
+    color = np.concatenate([np.random.random(3), np.array([0.8])], axis=0)
+
+    height, width = mask.shape[-2:]
+    mask_image = mask.reshape(height, width, 1) * color.reshape(1, 1, -1)
+
+    mask_image_pil = Image.fromarray(
+        (mask_image.cpu().numpy() * 255).astype(np.uint8)
+    ).convert("RGBA")
+
+    return Image.alpha_composite(image.convert("RGBA"), mask_image_pil)
