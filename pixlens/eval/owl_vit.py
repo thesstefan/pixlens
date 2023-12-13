ea329c1886985b9b97288e2beb4ae84f182ddf14
lluisp1999 <pastorlluis99@gmail.com>
2023-12-14 00:28:42 +0100

    Added versions of owlv2

--- pixlens/eval/owl_vit.py
+++ pixlens/eval/owl_vit.py
@@ -2,7 +2,7 @@ import logging
 import enum
 from pathlib import Path
 import os
-from transformers import OwlViTProcessor, OwlViTForObjectDetection
+from transformers import OwlViTProcessor, OwlViTForObjectDetection, Owlv2Processor, Owlv2ForObjectDetection
 
 from pixlens import utils
 
@@ -10,6 +10,8 @@ class OwlViTType(enum.StrEnum):
     base32 = "google/owlvit-base-patch32"
     base16 = "google/owlvit-base-patch16"
     large = "google/owlvit-large-patch14"
+    base16v2="google/owlv2-base-patch16"
+    largev2="google/owlv2-large-patch14"
 #They are automatically stored in users/$USER/.cache/huggingface/hub   
 
 def log_if_model_not_in_cache(model_name: str, cache_dir: Path) -> None:
@@ -24,8 +26,12 @@ def log_if_model_not_in_cache(model_name: str, cache_dir: Path) -> None:
 def load_owlvit(OwlViTType: OwlViTType.large , device: str = 'cpu'):
     path_to_cache = utils.get_cache_dir()
     log_if_model_not_in_cache(OwlViTType, path_to_cache)
-    processor = OwlViTProcessor.from_pretrained(OwlViTType, cache_dir=path_to_cache)
-    model = OwlViTForObjectDetection.from_pretrained(OwlViTType, cache_dir=path_to_cache)
+    if 'v2' in OwlViTType:
+        processor = Owlv2Processor.from_pretrained(OwlViTType, cache_dir=path_to_cache)
+        model = Owlv2ForObjectDetection.from_pretrained(OwlViTType, cache_dir=path_to_cache)
+    else:
+        processor = OwlViTProcessor.from_pretrained(OwlViTType, cache_dir=path_to_cache)
+        model = OwlViTForObjectDetection.from_pretrained(OwlViTType, cache_dir=path_to_cache)
     model.to(device)
     model.eval()
     return model, processor
