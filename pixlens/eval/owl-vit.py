882a820ece45804527b50ce50b72db3eccbd1933
lluisp1999 <pastorlluis99@gmail.com>
2023-12-13 19:50:19 +0100

    Added 3 types of OwlVit

--- pixlens/eval/owl-vit.py
+++ pixlens/eval/owl-vit.py
@@ -1,16 +1,23 @@
 import logging
+import enum
 
 import numpy as np
 import torch
 from transformers import OwlViTProcessor, OwlViTForObjectDetection
 
-
+from pixlens import utils
 from pixlens.eval import sam as eval_sam
 
-def load_owlvit(ckpt_path: str, device: str = 'cpu'):
-    processor = OwlViTProcessor.from_pretrained(ckpt_path)
-    model = OwlViTForObjectDetection.from_pretrained(ckpt_path)
+class OwlViTType(enum.StrEnum):
+    base32 = "google/owlvit-base-patch32"
+    base16 = "google/owlvit-base-patch16"
+    large = "google/owlvit-large-patch14"
+
+def load_owlvit(OwlViTType: OwlViTType.base32 , device: str = 'cpu'):
+    processor = OwlViTProcessor.from_pretrained(OwlViTType)
+    model = OwlViTForObjectDetection.from_pretrained(OwlViTType)
     model.to(device)
     model.eval()
     return model, processor
+
 
