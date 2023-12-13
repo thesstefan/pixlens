b222fe11cd99af8bc130eb471469f85a97463201
lluisp1999 <pastorlluis99@gmail.com>
2023-12-13 19:02:29 +0100

    Added argument of object detection. Submodule editval

--- pixlens/cli/pixlens_cli.py
+++ pixlens/cli/pixlens_cli.py
@@ -10,7 +10,7 @@ from pixlens.visualization import annotation
 parser = argparse.ArgumentParser(
     description="PixLens - Evaluate & understand image editing models"
 )
-
+parser.add_argument("--objectdetection", type=str, default="GroundedSAM", help=("Detector, either GroundedSAM or Owl-vitSAM"))
 parser.add_argument("--out", type=str, help=("Path of output annotated image"))
 parser.add_argument("--image", type=str, help=("Image to detect objects in"))
 parser.add_argument("--prompt", type=str, help=("Prompt to guide detection"))
@@ -20,21 +20,26 @@ def main() -> None:
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = grounded_sam.GroundedSAM(device=device)
-    segmentation_output, detection_output = model.detect_and_segment(
-        args.prompt, args.image
-    )
+    if args.objectdetection == "GroundedSAM":
+        model = grounded_sam.GroundedSAM(device=device)
+        segmentation_output, detection_output = model.detect_and_segment(
+            args.prompt, args.image
+        )
 
-    image_source = np.asarray(Image.open(args.image).convert("RGB"))
+        image_source = np.asarray(Image.open(args.image).convert("RGB"))
 
-    annotated_image = annotation.annotate_detection_output(
-        image_source, detection_output
-    )
+        annotated_image = annotation.annotate_detection_output(
+            image_source, detection_output
+        )
 
-    masked_annotated_image = annotation.annotate_mask(
-        segmentation_output.masks, annotated_image
-    )
-    masked_annotated_image.save(args.out)
+        masked_annotated_image = annotation.annotate_mask(
+            segmentation_output.masks, annotated_image
+        )
+        masked_annotated_image.save(args.out)
+    elif args.objectdetection == "Owl-vitSAM":
+        raise NotImplementedError
+    else:
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
