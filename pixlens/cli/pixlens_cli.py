88d67f3b742d71e4924343ba042ff304387345fc
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-12-15 04:11:27 +0100

    Fix line size requirement

--- pixlens/cli/pixlens_cli.py
+++ pixlens/cli/pixlens_cli.py
@@ -8,7 +8,7 @@ from pixlens.eval import grounded_sam
 from pixlens.visualization import annotation
 
 parser = argparse.ArgumentParser(
-    description="PixLens - Evaluate & understand image editing models"
+    description="PixLens - Evaluate & understand image editing models",
 )
 
 parser.add_argument("--out", type=str, help=("Path of output annotated image"))
@@ -22,17 +22,20 @@ def main() -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = grounded_sam.GroundedSAM(device=device)
     segmentation_output, detection_output = model.detect_and_segment(
-        args.prompt, args.image
+        args.prompt,
+        args.image,
     )
 
     image_source = np.asarray(Image.open(args.image).convert("RGB"))
 
     annotated_image = annotation.annotate_detection_output(
-        image_source, detection_output
+        image_source,
+        detection_output,
     )
 
     masked_annotated_image = annotation.annotate_mask(
-        segmentation_output.masks, annotated_image
+        segmentation_output.masks,
+        annotated_image,
     )
     masked_annotated_image.save(args.out)
 
