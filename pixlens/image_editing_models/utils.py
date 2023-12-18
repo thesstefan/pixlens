cc82f2ce83e9427029e88a842e2cfb427c28b111
lluisp1999 <pastorlluis99@gmail.com>
2023-12-18 21:03:27 +0100

    Changed lazy logging

--- pixlens/image_editing_models/utils.py
+++ pixlens/image_editing_models/utils.py
@@ -7,4 +7,7 @@ def log_model_if_not_in_cache(model_name: str, cache_dir: Path) -> None:
     model_dir = "models--" + model_dir
     full_path = cache_dir / model_dir
     if not full_path.is_dir():
-        logging.info(f"Downloading model from {model_name}...")
+        logging.info(
+            "Downloading model from %s ...",
+            model_name,
+        )
