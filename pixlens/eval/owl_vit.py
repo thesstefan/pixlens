1634c3f039cd5e10039c3aabec19086ef2a46528
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-12-19 04:41:23 +0100

    Simplify OwlViT and solve typing issues

--- pixlens/eval/owl_vit.py
+++ pixlens/eval/owl_vit.py
@@ -1,15 +1,15 @@
-import logging
 import enum
+import logging
 from pathlib import Path
 
-from transformers import (
-    OwlViTProcessor,
-    OwlViTForObjectDetection,
-    Owlv2Processor,
-    Owlv2ForObjectDetection,
-)
 import torch
 from PIL import Image
+from transformers import (
+    Owlv2ForObjectDetection,
+    Owlv2Processor,
+    OwlViTForObjectDetection,
+    OwlViTProcessor,
+)
 
 from pixlens import utils
 from pixlens.eval import interfaces
@@ -23,39 +23,45 @@ class OwlViTType(enum.StrEnum):
     LARGE_V2 = "google/owlv2-large-patch14"
 
 
-# They are automatically stored in users/$USER/.cache/huggingface/hub
-
-
-def log_if_model_not_in_cache(model_name: str, cache_dir: Path) -> None:
-    model_dir = model_name.replace("/", "--")
+def log_if_model_not_in_cache(owlvit_type: str, cache_dir: Path) -> None:
+    model_dir = owlvit_type.replace("/", "--")
     model_dir = "models--" + model_dir
-    # Construct the full path to the model folder within the cache
-    full_path = cache_dir / model_dir
-    # Check if the folder exists
-    if not full_path.is_dir():
-        logging.info(f"Downloading OwlViT model from {model_name}...")
+
+    if not (cache_dir / model_dir).is_dir():
+        logging.info("Downloading OwlViT %s...", owlvit_type)
 
 
-def load_owlvit(owlvit_type: OwlViTType, device: torch.device | None = None):
+def load_owlvit(
+    owlvit_type: OwlViTType,
+    device: torch.device | None = None,
+) -> tuple[
+    Owlv2ForObjectDetection | OwlViTForObjectDetection,
+    Owlv2Processor | OwlViTProcessor,
+]:
     path_to_cache = utils.get_cache_dir()
     log_if_model_not_in_cache(owlvit_type, path_to_cache)
-    if owlvit_type in [OwlViTType.BASE16_V2, OwlViTType.LARGE_V2]:
-        processor = Owlv2Processor.from_pretrained(
-            owlvit_type, cache_dir=path_to_cache
-        )
-        model = Owlv2ForObjectDetection.from_pretrained(
-            owlvit_type, cache_dir=path_to_cache
-        )
-    else:
-        processor = OwlViTProcessor.from_pretrained(
-            owlvit_type, cache_dir=path_to_cache
-        )
-        model = OwlViTForObjectDetection.from_pretrained(
-            owlvit_type, cache_dir=path_to_cache
-        )
+
+    model = (
+        Owlv2ForObjectDetection
+        if owlvit_type in [OwlViTType.BASE16_V2, OwlViTType.LARGE_V2]
+        else OwlViTForObjectDetection
+    ).from_pretrained(owlvit_type, cache_dir=path_to_cache)
+
+    processor = (
+        Owlv2Processor
+        if owlvit_type in [OwlViTType.BASE16_V2, OwlViTType.LARGE_V2]
+        else OwlViTProcessor
+    ).from_pretrained(owlvit_type, cache_dir=path_to_cache)
+
     model.to(device)
     model.eval()
-    return model, processor
+
+    # TODO: Fix this mypy error - there is probably something
+    #       wrong with the OwlViT stubs, but I can't figure it out now.
+    #
+    #       The Self return value in the class methods doesn't seem to
+    #       work correctly when inherited.
+    return model, processor  # type: ignore[return-value]
 
 
 class OwlViT(interfaces.PromptableDetectionModel):
@@ -71,27 +77,19 @@ class OwlViT(interfaces.PromptableDetectionModel):
         self.model, self.processor = load_owlvit(owlvit_type, device)
         self.detection_confidence_threshold = detection_confidence_threshold
 
-    def output_into_detection_output(
-        self, owlvit_results: list[dict], prompt: list[str]
-    ) -> list:
-        results_new = []
-        for result in owlvit_results:
-            scores = result["scores"]
-            labels = [prompt[id] for id in result["labels"].tolist()]
-            boxes = result["boxes"]
-
-            detection_output = interfaces.DetectionOutput(
-                bounding_boxes=boxes, logits=scores, phrases=labels
-            )
-            results_new.append(detection_output)
-        return results_new
-
-    def detect(self, prompt: str, image_path: str) -> list:
+    def detect(
+        self,
+        prompt: str,
+        image_path: str,
+    ) -> interfaces.DetectionOutput:
         image = Image.open(image_path)
         prompts = prompt.split(",")
         inputs = self.processor(
-            text=prompts, images=image, return_tensors="pt"
+            text=prompts,
+            images=image,
+            return_tensors="pt",
         ).to(self.device)
+
         with torch.no_grad():
             outputs = self.model(**inputs)
 
@@ -99,12 +97,10 @@ class OwlViT(interfaces.PromptableDetectionModel):
             outputs=outputs,
             threshold=self.detection_confidence_threshold,
             target_sizes=torch.Tensor([image.size[::-1]]).to(self.device),
+        )[0]
+
+        return interfaces.DetectionOutput(
+            logits=results["scores"].cpu(),
+            bounding_boxes=results["boxes"].cpu(),
+            phrases=[prompts[idx] for idx in results["labels"]],
         )
-        results = self.output_into_detection_output(results, prompts)
-
-        # Move all tensors to CPU
-        for result in results:
-            result.bounding_boxes = result.bounding_boxes.cpu()
-            result.logits = result.logits.cpu()
-
-        return results[0]
