c055bd4fe9e11deaa496cd741a8ba1b225dfbe01
Julen Costa <jucosta@student.ethz.ch>
2023-12-21 23:38:03 +0100

    some scripts to start developing the correct branch, please don't use this

---
+++ pixlens/evaluation/edit_dataset.py
@@ -0,0 +1,66 @@
+import json
+from typing import list
+
+import pandas as pd
+
+from pixlens.evaluation import interfaces
+
+
+# create a class that will parse a json object to get some edit instructions
+# as a init function it receives the json object file path
+# in the init it calls a private function that parses the json object
+class EvaluationPipeline:
+    def __init__(self, json_object_path: str, dataset_path: str) -> None:
+        self.json_object_path = json_object_path
+        self.edit_dataset: pd.DataFrame
+        self.dataset_path = dataset_path
+        self._parse_json_object()
+
+    def _parse_json_object(self) -> None:
+        with open(self.json_object_path) as json_file:
+            json_data = json.load(json_file)
+
+        records: list[dict] = []
+
+        # Iterate through the JSON data
+        for obj_class, images in json_data.items():
+            for image_id, edits in images.items():
+                for edit_type, values in edits.items():
+                    from_values = values.get("from", [""])
+                    to_values = values.get("to", [])
+
+                    # Handle case when there is no "from" value
+                    if not from_values:
+                        from_values = [""] * len(to_values)
+
+                    # Iterate through "to" values
+                    for from_val, to_val in zip(from_values, to_values):
+                        records.append(
+                            {
+                                "edit_id": len(records),
+                                "image_id": image_id,
+                                "class": obj_class,
+                                "edit_type": edit_type,
+                                "from": from_val,
+                                "to": to_val,
+                                "edit_path": self.dataset_path + "/" + image_id,
+                            }
+                        )
+
+        # Create a pandas DataFrame from the records
+        self.edit_dataset = pd.DataFrame(records)
+
+    def __print__(self) -> None:
+        print(self.edit_dataset.head())
+
+    # getter edit from edit id
+    def get_edit_from_edit_id(self, edit_id: int) -> interfaces.Edit:
+        edit = self.edit_dataset.loc[edit_id]
+        return interfaces.Edit(
+            edit_id=edit["edit_id"],
+            image_id=edit["image_id"],
+            category=edit["class"],
+            edit_type=interfaces.EditType(edit["edit_type"]),
+            _from=edit["from"],
+            _to=edit["to"],
+        )
--- pixlens/evaluation/interfaces.py
+++ pixlens/evaluation/interfaces.py
@@ -1,13 +1,45 @@
 import dataclasses
+import enum
 from typing import Protocol
 
 from pixlens.detection.interfaces import DetectionSegmentationResult
 from pixlens.editing.interfaces import ImageEditingOutput
 
 
+class EditType(enum.StrEnum):
+    SIZE = "size"
+    COLOR = "color"
+    OBJECT_CHANGE = "object_change"
+    OBJECT_ADDITION = "object_addition"
+    POSITIONAL_ADDITION = "positional_addition"
+    OBJECT_REMOVAL = "object_removal"
+    OBJECT_REPLACEMENT = "object_replacement"
+    POSITION_REPLACEMENT = "position_replacement"
+    OBJECT_DUPLICATION = "object_duplication"
+    TEXTURE = "texture"
+    ACTION = "action"
+    VIEWPOINT = "viewpoint"
+    BACKGROUND = "background"
+    STYLE = "style"
+    SHAPE = "shape"
+    ALTER_PARTS = "alter_parts"
+
+
 @dataclasses.dataclass
 class EvaluationOutput:
     score: float
+
+
+@dataclasses.dataclass
+class Edit:
+    edit_id: int
+    # prompt: str
+    image_path: str
+    image_id: int
+    category: str
+    edit_type: EditType
+    _from: str
+    _to: str
 
 
 class OperationEvaluation(Protocol):
