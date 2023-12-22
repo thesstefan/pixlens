0c4886756d15395fa1281a4f2f7f25b31a2b26d8
lluisp1999 <pastorlluis99@gmail.com>
2023-12-22 23:24:24 +0100

    Got wrong model from blip1

--- pixlens/detection/automatic_label/blip.py
+++ pixlens/detection/automatic_label/blip.py
@@ -1,21 +1,19 @@
 import enum
-from pathlib import Path
 
 import torch
 from transformers import (
-    Blip2Processor,
-    Blip2ForConditionalGeneration,
     AutoProcessor,
-    BlipModel,
+    Blip2ForConditionalGeneration,
+    Blip2Processor,
+    BlipForConditionalGeneration,
 )
-
 
 from pixlens.detection.automatic_label.interfaces import (
-    ImageDescriptorModel,
     ImageDescription,
+    ImageDescriptorModel,
 )
 from pixlens.detection.utils import log_if_hugging_face_model_not_in_cache
-from pixlens.utils import get_cache_dir
+from pixlens.utils.utils import get_cache_dir
 
 
 class BlipType(enum.StrEnum):
@@ -27,7 +25,7 @@ def load_blip(
     blip_type: BlipType,
     device: torch.device | None = None,
 ) -> tuple[
-    Blip2ForConditionalGeneration | BlipModel,
+    Blip2ForConditionalGeneration | BlipForConditionalGeneration,
     Blip2Processor | AutoProcessor,
 ]:
     log_if_hugging_face_model_not_in_cache(blip_type)
@@ -35,7 +33,7 @@ def load_blip(
     model = (
         Blip2ForConditionalGeneration
         if blip_type in [BlipType.BLIP2]
-        else BlipModel
+        else BlipForConditionalGeneration
     ).from_pretrained(blip_type, cache_dir=path_to_cache)
     processor = (
         Blip2Processor if blip_type in [BlipType.BLIP2] else AutoProcessor
@@ -46,8 +44,9 @@ def load_blip(
     return model, processor
 
 
-class BlipModel(ImageDescriptorModel):
+class Blip(ImageDescriptorModel):
     device: torch.device | None
+    model: torch.nn.Module
 
     def __init__(
         self,
@@ -55,3 +54,4 @@ class BlipModel(ImageDescriptorModel):
         device: torch.device | None = None,
     ) -> None:
         self.device = device
+        self.model, self.processor = load_blip(blip_type, device)
