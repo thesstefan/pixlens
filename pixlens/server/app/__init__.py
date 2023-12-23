8d0f74c29f8e89262d56f3da69bed1305f67ef19
Stefan Stefanache <stefanstefanache@protonmail.com>
2023-12-23 03:15:36 +0200

    Implement server inference for InstructPix2Pix, ControlNet and OwlViTSAM

--- pixlens/server/app/__init__.py
+++ pixlens/server/app/__init__.py
@@ -2,13 +2,23 @@ import flask
 
 from pixlens.server.app import ml_models
 from pixlens.server.app.routes import bp
+from pixlens.server.config import Config
 
 
 def create_app() -> flask.Flask:
     app = flask.Flask(__name__)
     app.register_blueprint(bp)
+    app.config.from_object(Config)
 
-    # Load moedl and cache it
-    ml_models.get_detect_and_segment_model()
+    config = Config()
+
+    if config.DETECT_SEGMENT_MODEL:
+        ml_models.get_detect_segment_model(
+            config.DETECT_SEGMENT_MODEL,
+            config.DEVICE,
+        )
+
+    if config.EDIT_MODEL:
+        ml_models.get_edit_model(config.EDIT_MODEL, config.DEVICE)
 
     return app
--- pixlens/server/app/routes.py
+++ pixlens/server/app/routes.py
@@ -1,11 +1,14 @@
 import io
+import pathlib
 
 import flask
 import numpy as np
 from PIL import Image
+from werkzeug.datastructures import FileStorage
 
 from pixlens.server.app import ml_models
-from pixlens.utils import get_cache_dir
+from pixlens.server.config import Config
+from pixlens.utils.utils import get_cache_dir
 from pixlens.visualization import annotation
 
 bp = flask.Blueprint(
@@ -14,21 +17,56 @@ bp = flask.Blueprint(
     url_prefix="/pixlens",
 )
 
+config = Config()
 
-@bp.route("/detect_and_segment", methods=["POST"])
-def detect_and_segment() -> flask.Response:
-    image_file = flask.request.files["image"]
-    prompt = flask.request.form["prompt"]
 
-    image_bytes = image_file.read()
+def save_tmp_image(  # type: ignore[no-any-unimported]
+    file: FileStorage,
+) -> tuple[pathlib.Path, Image.Image]:
+    image_bytes = file.read()
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
 
-    # TODO(thesstefan): This is incredibly awkward - change models
-    #                   to take an input image instead of a path
+    # TODO: This is incredibly awkward - change models
+    #       to take an input image instead of a path
     image_path = get_cache_dir() / "tmp.png"
     image.save(image_path)
 
-    model = ml_models.get_detect_and_segment_model()
+    return image_path, image
+
+
+def create_image_response(image: Image.Image) -> flask.Response:
+    image_bytes = io.BytesIO()
+    image.save(image_bytes, "png", quality=100)
+    image_bytes.seek(0)
+
+    return flask.send_file(image_bytes, mimetype="image/png")
+
+
+@bp.route("/edit", methods=["POST"])
+def edit() -> flask.Response:
+    image_path, image = save_tmp_image(flask.request.files["image"])
+    prompt = flask.request.form["prompt"]
+
+    model = ml_models.get_edit_model(
+        config.EDIT_MODEL,
+        config.DEVICE,
+    )
+    edit_output = model.edit(prompt, str(image_path))
+
+    image_path.unlink()
+
+    return create_image_response(edit_output.image)
+
+
+@bp.route("/detect_and_segment", methods=["POST"])
+def detect_and_segment() -> flask.Response:
+    image_path, image = save_tmp_image(flask.request.files["image"])
+    prompt = flask.request.form["prompt"]
+
+    model = ml_models.get_detect_segment_model(
+        config.DETECT_SEGMENT_MODEL,
+        config.DEVICE,
+    )
     (
         segmentation_output,
         detection_output,
@@ -46,8 +84,4 @@ def detect_and_segment() -> flask.Response:
         annotated_image,
     )
 
-    mask_annotated_bytes = io.BytesIO()
-    masked_annotated_image.save(mask_annotated_bytes, "png", quality=100)
-    mask_annotated_bytes.seek(0)
-
-    return flask.send_file(mask_annotated_bytes, mimetype="image/png")
+    return create_image_response(masked_annotated_image)
