111a49070134a1f10da360dc5376dc22150fe128
lluisp1999 <pastorlluis99@gmail.com>
2023-12-23 13:10:30 +0100

    Changed structure of edit function in promptable model

--- pixlens/editing/interfaces.py
+++ pixlens/editing/interfaces.py
@@ -1,7 +1,6 @@
-import dataclasses
 import logging
 from abc import abstractmethod
-from typing import Protocol
+from typing import Protocol, Tuple
 from pathlib import Path
 
 from PIL import Image
@@ -18,18 +17,31 @@ class PromptableImageEditingModel(Protocol):
     def edit_image(self, prompt: str, image_path: str) -> Image.Image:
         ...
 
-    def edit(self, prompt: str, image_path: str) -> Image.Image:
+    def check_if_image_exists(
+        self, prompt: str, image_path: str
+    ) -> Tuple[bool, Path]:
+        """
+        Check if the image exists and return a tuple of (bool, path).
+
+        The bool value is True if the image exists, and the path is where the image is stored.
+        """
         cache_dir = utils.get_cache_dir()
         model_dir = self.get_model_name().replace("/", "--")
         model_dir = "models--" + model_dir
         full_path = cache_dir / model_dir / Path(image_path).stem / prompt
         full_path = full_path.with_suffix(".png")
-        if full_path.is_file():
-            logging.info("Using cached edited image")
-            return Image.open(full_path)
+        return full_path.exists(), full_path
 
-        logging.info("Editing image...")
-        edited_image = self.edit_image(prompt, image_path)
-        full_path.parent.mkdir(parents=True, exist_ok=True)
-        edited_image.save(full_path)
+    def edit(self, prompt: str, image_path: str) -> Image.Image:
+        image_exists_bool, path_of_image = self.check_if_image_exists(
+            prompt, image_path
+        )
+        if image_exists_bool:
+            logging.info("Image already exists, loading...")
+            edited_image = Image.open(path_of_image)
+        else:
+            logging.info("Editing image...")
+            edited_image = self.edit_image(prompt, image_path)
+            path_of_image.parent.mkdir(parents=True, exist_ok=True)
+            edited_image.save(path_of_image)
         return edited_image
