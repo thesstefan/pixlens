de8d914facb335115f0dff0501b53141bc79dd02
Julen Costa <jucosta@student.ethz.ch>
2023-12-26 00:09:02 +0100

    Initial version of color operation evaluation implemented. requirements.txt modified accordingly

---
+++ pixlens/evaluation/operations/color.py
@@ -0,0 +1,34 @@
+from pixlens.evaluation import interfaces as evaluation_interfaces
+from pixlens.evaluation.utils import color_change_applied_correctly
+
+
+class ColorEdit(evaluation_interfaces.OperationEvaluation):
+    def evaluate_edit(
+        self,
+        evaluation_input: evaluation_interfaces.EvaluationInput,
+    ) -> evaluation_interfaces.EvaluationOutput:
+        input_segmentation = evaluation_input.input_detection_segmentation_result.segmentation_output
+        edit_segmentation = evaluation_input.edited_detection_segmentation_result.segmentation_output
+        if not input_segmentation.masks.any():
+            return evaluation_interfaces.EvaluationOutput(
+                score=-1.0,
+                success=False,
+            )  # Object wasn't even present at input
+        if edit_segmentation.masks:
+            target_color = evaluation_input.edit.to_attribute
+            idmax = edit_segmentation.logits.argmax()
+            mask_edited = edit_segmentation.masks[idmax]
+
+            if color_change_applied_correctly(
+                image=evaluation_input.edited_image,
+                mask=mask_edited,
+                target_color=target_color,
+            ):
+                return evaluation_interfaces.EvaluationOutput(
+                    score=1.0,
+                    success=True,
+                )
+        return evaluation_interfaces.EvaluationOutput(
+            score=0.0,
+            success=True,
+        )  # Object wasn't present at output or color was not changed correctly
--- pixlens/evaluation/utils.py
+++ pixlens/evaluation/utils.py
@@ -1,4 +1,10 @@
+import colorspacious as cs
+import numpy as np
 import torch
+from PIL import Image, ImageColor
+
+# import delta e color similarity function
+from skimage.color import delta_e_cie2000
 
 from pixlens.evaluation.interfaces import Edit, EditType
 
@@ -76,6 +82,15 @@ def compute_iou(tensor1: torch.Tensor, tensor2: torch.Tensor) -> float:
     return iou.item()
 
 
+def rgb_to_lab(rgb_color: np.ndarray) -> np.ndarray:
+    # Convert RGB to LAB
+    return cs.cspace_convert(
+        rgb_color,
+        start={"name": "sRGB1"},
+        end={"name": "CIELab"},
+    )
+
+
 def is_small_area_within_big_area(
     input_mask: torch.Tensor,
     edited_mask: torch.Tensor,
@@ -93,3 +108,36 @@ def is_small_area_within_big_area(
         raise ValueError(SHAPE_DIFFERENCE_MSG)
     intersection = torch.logical_and(small_area, big_area).sum()
     return intersection.item() / compute_area(small_area) > confidence_threshold
+
+
+# TODO: this function is clearly limited and should not be used in the final
+# definitive version. Imagine the target color is "ocean blue"
+# and the 5 closest colors are "blue", "light blue", "dark blue", "sky blue",
+# "blueberry". The function will return False because "ocean blue" is not in
+# the 5 closest colors. However, the edit is still correct.
+def color_change_applied_correctly(
+    image: Image.Image,
+    mask: torch.Tensor,
+    target_color: str,
+) -> bool:
+    # get the colors in the image but only on the mask area
+    colors = image.crop(mask).getcolors(image.size[0] * image.size[1])
+
+    # Get the most common color
+    dominant_color = rgb_to_lab(max(colors, key=lambda x: x[0])[1])
+
+    # Find the 5 colors in PIL.ImageColor.colormap that are most similar
+    # to the retrieved color based on Delta-E distance
+    # however the colors in the colormap are in hexadecimal and the dominant
+    # color is in RGB, so we need to convert all colors to Lab space as it is
+    # required by the delta_e_cie2000 function
+    closest_colors = sorted(
+        ImageColor.colormap.items(),
+        key=lambda color: delta_e_cie2000(
+            rgb_to_lab(ImageColor.getrgb(color[1])),
+            dominant_color,
+        ),
+    )[:5]
+
+    # Check if the target color is in the 5 closest colors
+    return target_color in [color[0] for color in closest_colors]
