7e283f2e118dd70ed07d576977e240a4657c9095
Julen Costa <jucosta@student.ethz.ch>
2023-12-30 17:46:24 +0100

    from YAML argument name error

--- pixlens/cli/pixlens_editing_cli.py
+++ pixlens/cli/pixlens_editing_cli.py
@@ -59,7 +59,7 @@ def main() -> None:
         in_path = args.input
         prompt = args.prompt
 
-    model = NAME_TO_MODEL[args.model].from_yaml(args.params_yaml)
+    model = NAME_TO_MODEL[args.model].from_yaml(args.model_params_yaml)
 
     output = model.edit(prompt, in_path)
     if args.input is None:
