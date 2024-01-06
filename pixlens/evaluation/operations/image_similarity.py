51954b8b8e94c7d463ecaf3888e17a96370abf4d
lluisp1999 <pastorlluis99@gmail.com>
2024-01-06 20:02:36 +0100

    Added function apply_Segmentation_mask to black all pixels in a given segmentation mask. Added masked_image_similarity to basically compare images without the segmentation masks

--- pixlens/evaluation/operations/image_similarity.py
+++ pixlens/evaluation/operations/image_similarity.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 from sklearn.metrics import mean_squared_error
@@ -25,3 +26,52 @@ def image_similarity(img1: Image.Image, img2: Image.Image) -> float:
     return (
         1 - mse_value
     ) * ssim_value  # (We can modify this as we want honestly)
+
+
+def apply_segmentation_mask(
+    image: Image.Image,
+    mask: torch.Tensor,
+) -> Image.Image:
+    """Apply a segmentation mask to an image, coloring the masked parts black.
+
+    :param image: PIL.Image - The original image
+    :param mask: np.array - A boolean array representing the segmentation mask
+    :return: PIL.Image - The image with the segmentation applied
+    """
+    # Convert the image to a numpy array
+    img_array = np.array(image)
+
+    # Ensure the mask is a boolean array
+    mask = mask.cpu().numpy().astype(bool)
+
+    # Check if dimensions of the mask and the image match
+    if img_array.shape[:2] != mask.shape[-2:]:
+        raise ValueError(
+            "The dimensions of the image and the mask do not match."
+        )
+
+    # Apply the mask: set pixels to black where mask is True
+    img_array[mask] = [0, 0, 0]
+
+    # Convert the array back to an image
+    return Image.fromarray(img_array)
+
+
+def compute_union_segmentation_masks(
+    mask1: torch.Tensor,
+    mask2: torch.Tensor,
+) -> torch.Tensor:
+    raise NotImplementedError
+
+
+def masked_image_similarity(
+    image1: Image.Image,
+    mask1: torch.Tensor,
+    image2: Image.Image,
+    mask2: torch.Tensor,
+) -> float:
+    new_mask = compute_union_segmentation_masks(mask1, mask2)
+    masked_image1 = apply_segmentation_mask(image1, new_mask)
+    masked_image2 = apply_segmentation_mask(image2, new_mask)
+    return image_similarity(masked_image1, masked_image2)
+
