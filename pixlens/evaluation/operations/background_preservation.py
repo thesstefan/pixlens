66c77c2e6a1685f7594cd3beb28c006bc71f7efe
lluisp1999 <pastorlluis99@gmail.com>
2024-01-06 20:30:31 +0100

    Added the first part of get_masks

--- pixlens/evaluation/operations/background_preservation.py
+++ pixlens/evaluation/operations/background_preservation.py
@@ -1,3 +1,5 @@
+import torch
+
 from pixlens.evaluation import interfaces as evaluation_interfaces
 from pixlens.evaluation.operations import image_similarity
 
@@ -9,5 +11,11 @@ class BackgroundPreservation(evaluation_interfaces.GeneralEvaluation):
     ) -> float:
         input_image = evaluation_input.input_image
         edited_image = evaluation_input.edited_image
-
+        mask_input, mask_edit = self.get_masks(evaluation_input)
         return 0.0
+
+    def get_masks(
+        self,
+        evaluation_input: evaluation_interfaces.EvaluationInput,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        raise NotImplementedError
