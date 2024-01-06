67f62e2a3cd67e8877be8a90aee700a7e27a5927
Julen Costa <jucosta@student.ethz.ch>
2024-01-06 21:03:15 +0100

    fixed the issue, also added minimum required size change threshold for a size change edit to be considered successful

--- pixlens/evaluation/operations/size.py
+++ pixlens/evaluation/operations/size.py
@@ -1,7 +1,9 @@
+import numpy as np
 import torch
 
 from pixlens.evaluation import interfaces as evaluation_interfaces
 from pixlens.evaluation.utils import (
+    center_of_mass,
     compute_area_ratio,
     is_small_area_within_big_area,
 )
@@ -11,23 +13,18 @@ def size_change_applied_correctly(
     direction: str,
     input_mask: torch.Tensor,
     output_mask: torch.Tensor,
+    minimum_required_change: float = 0.1,
 ) -> bool:
     if direction == "small":
-        return (
-            compute_area_ratio(
-                numerator=output_mask,
-                denominator=input_mask,
-            )
-            < 1
-        )
+        return compute_area_ratio(
+            numerator=output_mask,
+            denominator=input_mask,
+        ) < (1 - minimum_required_change)
     if direction == "big":
-        return (
-            compute_area_ratio(
-                numerator=output_mask,
-                denominator=input_mask,
-            )
-            > 1
-        )
+        return compute_area_ratio(
+            numerator=output_mask,
+            denominator=input_mask,
+        ) > (1 + minimum_required_change)
     error_msg = f"Invalid direction: {direction}"
     raise ValueError(error_msg)
 
@@ -49,14 +46,38 @@ class SizeEdit(evaluation_interfaces.OperationEvaluation):
             # Code continues here...
             # 2 - Check if resize is small or big and compute area difference
             transformation = evaluation_input.edit.to_attribute
+
+            # get center of mass of the category in the input image
             mask_input = input_segmentation.masks[0]
-            idmax = edit_segmentation.logits.argmax()
-            mask_edited = edit_segmentation.masks[idmax]
+            category_center_of_mass = center_of_mass(
+                mask_input,
+            )
+
+            # retrieve index of the object in edited image that is closer to the input object
+            # distance is computed as euclidean distance between the center of mass of the
+            # category in the input image and the center of mass of the object in the edited image
+            edited_centers_of_mass = [
+                center_of_mass(mask) for mask in edit_segmentation.masks
+            ]
+
+            closest_obj_index = int(
+                np.argmin(
+                    [
+                        np.linalg.norm(
+                            np.array(category_center_of_mass)
+                            - np.array(edited_center_of_mass),
+                        )
+                        for edited_center_of_mass in edited_centers_of_mass
+                    ],
+                ),
+            )
+
+            mask_edited = edit_segmentation.masks[closest_obj_index]
 
             if size_change_applied_correctly(
                 transformation,
-                mask_input,
-                mask_edited,
+                input_mask=mask_input,
+                output_mask=mask_edited,
             ):
                 return evaluation_interfaces.EvaluationOutput(
                     edit_specific_score=float(
