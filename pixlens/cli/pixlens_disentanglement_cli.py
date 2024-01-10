b90befdbda3f21238cb19368ba0d842bd8190681
lluisp1999 <pastorlluis99@gmail.com>
2024-01-10 20:43:53 +0100

    Loading models with yaml

--- pixlens/cli/pixlens_disentanglement_cli.py
+++ pixlens/cli/pixlens_disentanglement_cli.py
@@ -2,7 +2,11 @@ import argparse
 
 import torch
 
-from pixlens.editing import controlnet, instruct_pix2pix
+from pixlens.editing import (
+    controlnet,
+    instruct_pix2pix,
+    load_editing_model_from_yaml,
+)
 from pixlens.editing.interfaces import PromptableImageEditingModel
 from pixlens.evaluation.operations.disentanglement_operation.disentanglement import (
     Disentanglement,
@@ -10,30 +14,21 @@ from pixlens.evaluation.operations.disentanglement_operation.disentanglement imp
 
 parser = argparse.ArgumentParser(description="Evaluate PixLens Editing Model")
 parser.add_argument(
-    "--editing-model",
+    "--model_params_yaml",
     type=str,
+    help=("Path to YAML containing the model configuration"),
     required=True,
-    help="Name of the editing model to use",
 )
 
 
 def main() -> None:
     args = parser.parse_args()
-    device = torch.device("cuda")
     model: PromptableImageEditingModel
     disentangle = Disentanglement(
         json_file_path="disentanglement_json/objects_textures_sizes_colors_styles_test.json",
         image_data_path="editval_instances",
     )
-    if args.editing_model == "InstructPix2Pix":
-        model = instruct_pix2pix.InstructPix2Pix(
-            instruct_pix2pix.InstructPix2PixType.BASE,
-            device,
-        )
-    elif args.editing_model == "controlnet":
-        model = controlnet.ControlNet(controlnet.ControlNetType.BASE, device)
-    else:
-        raise NotImplementedError
+    model = load_editing_model_from_yaml(args.model_params_yaml)
     disentangle.evaluate_model(model=model)
 
 
