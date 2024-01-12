c7571d0645cce6d64f086d8970bbdf6f07033f44
Stefan Stefanache <stefanstefanache@protonmail.com>
2024-01-12 14:50:08 +0100

    Remove outdated color utils from evaluation.utils

--- pixlens/evaluation/utils.py
+++ pixlens/evaluation/utils.py
@@ -1,15 +1,13 @@
-from collections import Counter
-import cv2
 
+import cv2
 import numpy as np
-import torch
 import numpy.typing as npt
+import torch
 import torch.nn.functional as F  # noqa: N812
 from numpy.typing import NDArray
-from PIL import Image, ImageColor
+from PIL import Image
 
 # import delta e color similarity function
-from skimage.color import deltaE_ciede2000, rgb2lab
 from skimage.metrics import structural_similarity as ssim
 
 from pixlens.evaluation.interfaces import Edit, EditType, EvaluationInput
@@ -107,65 +105,6 @@ def is_small_area_within_big_area(
         big_area = edited_mask
     intersection = torch.logical_and(small_area, big_area).sum()
     return intersection.item() / compute_area(small_area) > confidence_threshold
-
-
-def get_colors_in_masked_area(
-    image: Image.Image,
-    mask: torch.Tensor,
-) -> list[tuple[int, int, int]]:
-    image_array = np.array(image)
-
-    boolean_mask = mask[0].cpu().numpy()
-    # cropped_array = image_array * boolean_mask[:, :, np.newaxis]
-    cropped_array = image_array[boolean_mask]
-
-    pixels = cropped_array.reshape(-1, cropped_array.shape[-1])
-
-    # Convert the 2D array to a list of tuples
-    pixel_tuples = [tuple(pixel) for pixel in pixels]
-
-    # Use Counter to count occurrences of each unique color tuple
-    color_counts = Counter(pixel_tuples)
-
-    # Convert the Counter items to an array of tuples (count, color)
-    return [(count, color) for color, count in color_counts.items()]
-
-
-# TODO: this function is clearly limited and should not be used in the final
-# definitive version. Imagine the target color is "ocean blue"
-# and the 5 closest colors are "blue", "light blue", "dark blue", "sky blue",
-# "blueberry". The function will return False because "ocean blue" is not in
-# the 5 closest colors. However, the edit is still correct.
-def color_change_applied_correctly(
-    image: Image.Image,
-    mask: torch.Tensor,
-    target_color: str,
-) -> bool:
-    # Get the colors in the masked area
-    colors = get_colors_in_masked_area(image, mask)
-
-    # colors_pil_method = image.getcolors(image.size[0] * image.size[1])
-    # assert that colors and colors_pil_method contain the same keys and values (order doesn't matter)
-    # assert Counter(colors) == Counter(colors_pil_method)
-
-    # Get the most common color
-    dominant_color = rgb2lab(max(colors, key=lambda x: x[0])[1])
-
-    # Find the 5 colors in PIL.ImageColor.colormap that are most similar
-    # to the retrieved color based on Delta-E distance
-    # however the colors in the colormap are in hexadecimal and the dominant
-    # color is in RGB, so we need to convert all colors to Lab space as it is
-    # required by the delta_e_cie2000 function
-    closest_colors = sorted(
-        ImageColor.colormap.items(),
-        key=lambda color: deltaE_ciede2000(
-            rgb2lab(ImageColor.getrgb(color[1])),
-            dominant_color,
-        ),
-    )[:10]
-
-    # Check if the target color is in the 5 closest colors
-    return target_color in [color[0] for color in closest_colors]
 
 
 def apply_mask(np_image: NDArray, mask: NDArray) -> NDArray:
