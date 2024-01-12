78c547971cec7a6f2b9c13cd4ac59fc55265fda9
Stefan Stefanache <stefanstefanache@protonmail.com>
2024-01-12 09:19:55 +0100

    Add utilities for selecting one subject out of multiple based on some criteria

---
+++ pixlens/evaluation/multiplicity_resolver.py
@@ -0,0 +1,70 @@
+import enum
+
+import numpy as np
+import numpy.typing as npt
+import torch
+
+from pixlens.evaluation.utils import center_of_mass
+
+
+class MultiplicityResolution(enum.StrEnum):
+    LARGEST = "LARGEST"
+    MOST_CONFIDENT = "MOST_CONFIDENT"
+    CLOSEST = "CLOSEST"
+
+
+def select_largest_2d(masks_2d: npt.NDArray) -> int:
+    assert len(np.squeeze(masks_2d)) == 3  # noqa: S101, PLR2004
+
+    return np.argmax(np.count_nonzero(masks_2d, axis=(-2, -1))).item()
+
+
+def select_closest_2d(masks_2d: npt.NDArray, relative: npt.NDArray) -> int:
+    # TODO: Update center_of_mass to use np arrays and not depend on shape
+
+    assert len(np.squeeze(masks_2d)) == 3  # noqa: S101, PLR2004
+    assert len(np.squeeze(relative)) == 2  # noqa: S101, PLR2004
+
+    if len(masks_2d.shape) == 3:  # noqa: PLR2004
+        masks_2d = np.expand_dims(masks_2d, 1)
+
+    if len(relative.shape) == 2:  # noqa: PLR2004
+        relative = np.expand_dims(relative, 0)
+
+    relative_center = np.array(center_of_mass(torch.Tensor(relative)))
+    mask_centers = [
+        np.array(center_of_mass(torch.Tensor(mask))) for mask in masks_2d
+    ]
+
+    return np.argmax(
+        [
+            np.linalg.norm(mask_center - relative_center)
+            for mask_center in mask_centers
+        ],
+    ).item()
+
+
+# Kind of stupid, but allows us to be certain that the same thing
+# is done for all operations.
+def select_one_2d(
+    masks_2d: npt.NDArray,
+    resolution: MultiplicityResolution,
+    confidences: npt.NDArray | None = None,
+    relative_mask: npt.NDArray | None = None,
+) -> int:
+    match resolution:
+        case MultiplicityResolution.LARGEST:
+            return select_largest_2d(masks_2d)
+        case MultiplicityResolution.MOST_CONFIDENT:
+            assert confidences  # noqa: S101
+            assert len(masks_2d) == len(confidences)  # noqa: S101
+            assert len(confidences.shape) == 1  # noqa: S101
+
+            return np.argmax(confidences).item()
+        case MultiplicityResolution.CLOSEST:
+            assert relative_mask  # noqa: S101
+
+            return select_closest_2d(masks_2d, relative_mask)
+        case _:
+            msg = f"{resolution} is not a supported resolution type!"
+            raise RuntimeError(msg)
