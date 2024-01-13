26e29fb4087e9057925eaa5e8fcad84a459b8953
lluisp1999 <pastorlluis99@gmail.com>
2024-01-13 19:35:14 +0100

    Added edit_image, still not working

--- pixlens/editing/diffedit_v2.py
+++ pixlens/editing/diffedit_v2.py
@@ -1,4 +1,5 @@
 import torch
+from PIL import Image
 
 from pixlens.editing import interfaces as editing_interfaces
 from pixlens.editing.impl.diffedit.diffedit import diffedit
@@ -18,3 +19,30 @@ class DiffEdit(editing_interfaces.PromptableImageEditingModel):
         self.device = device
         self.seed = seed
         self.ddim_steps = ddim_steps
+
+    @property
+    def params_dict(self) -> dict[str, str | bool | int | float]:
+        return {
+            "device": str(self.device),
+            "ddim_steps": self.ddim_steps,
+            "seed": self.seed,
+        }
+
+    def edit_image(
+        self,
+        prompt: str,
+        image_path: str,
+        edit_info: Edit | None = None,
+    ) -> Image.Image:
+        del edit_info
+
+        source_prompt, target_prompt = prompt.split("[SEP]")
+        images, _ = diffedit(
+            self.model,
+            image_path,
+            src_prompt=source_prompt,
+            tgt_prompt=target_prompt,
+            ddim_steps=self.ddim_steps,
+            seed=self.seed,
+        )
+        
