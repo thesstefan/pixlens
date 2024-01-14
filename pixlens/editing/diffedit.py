3ee1a3593c0358310ee71901b670835530ec99d4
lluisp1999 <pastorlluis99@gmail.com>
2024-01-14 02:53:16 +0100

    Forgot to remove reductions on prompts

--- pixlens/editing/diffedit.py
+++ pixlens/editing/diffedit.py
@@ -91,8 +91,6 @@ class DiffEdit(interfaces.PromptableImageEditingModel):
         del edit_info
         self.device = torch.device("cuda")  # to delete
         source_prompt, target_prompt = prompt.split("[SEP]")
-        source_prompt = source_prompt[13:]
-        target_prompt = target_prompt[13:]
         images, _ = diffedit(
             self.model,
             image_path,
