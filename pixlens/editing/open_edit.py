7b54768eb8f5b8d3d5ef3570753e81dcc83db21c
lluisp1999 <pastorlluis99@gmail.com>
2024-02-22 01:49:03 +0100

    Implemented open_edit.py, soon to be as in the other imageeditingmodel

--- pixlens/editing/open_edit.py
+++ pixlens/editing/open_edit.py
@@ -1,68 +1,112 @@
-import enum
+import pickle
+from pathlib import Path
 
-import cv2  # type: ignore[import]
-import numpy as np
 import torch
 from PIL import Image
+from torchvision import transforms
 
-from pixlens.editing import interfaces
-from pixlens.editing.utils import (
-    generate_instruction_based_prompt,
-    log_model_if_not_in_cache,
+from pixlens.editing.impl.open_edit.options.train_options import TrainOptions
+from pixlens.editing.impl.open_edit.trainers.OpenEdit_optimizer import (
+    OpenEditOptimizer,
 )
-from pixlens.evaluation.interfaces import Edit
-from pixlens.utils import utils
+from pixlens.editing.impl.open_edit.util.visualizer import Visualizer
 
 
-class (interfaces.PromptableImageEditingModel):
-    model: StableDiffusionControlNetPipeline
-    controlnet_type: ControlNetType
-    device: torch.device | None
-    num_inference_steps: int
-    image_guidance_scale: float
-    text_guidance_scale: float
+class OpenEditEditingModel:
+    def __init__(self) -> None:
+        opt = TrainOptions().parse()
+        opt.gpu = 0
+        self.global_edit = False
+        self.alpha = 5
+        self.optimize_iter = 10
+        opt.world_size = 1
+        opt.rank = 0
+        opt.mpdist = False
+        opt.num_gpu = 1
+        opt.batchSize = 1
+        opt.manipulation = True
+        opt.perturbation = True
+        self.opt = opt
+        self.open_edit_optimizer = OpenEditOptimizer(opt)
+        self.open_edit_optimizer.open_edit_model.netG.eval()
+        self.visualizer = Visualizer(opt, rank=0)
 
-    def __init__(  # noqa: PLR0913
+    def image_loader(self, image_path: str) -> torch.Tensor:
+        transforms_list = []
+        transforms_list.append(
+            transforms.Resize((self.opt.img_size, self.opt.img_size))
+        )
+        transforms_list += [transforms.ToTensor()]
+        transforms_list += [
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]
+        transform = transforms.Compose(transforms_list)
+        image = Image.open(image_path).convert("RGB")
+        image_tensor = transform(image)
+        return image_tensor.unsqueeze(0).cuda()
+
+    def text_loader(
+        self, ori_cap_str: str, new_cap_str: str
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        ori_cap = ori_cap_str.split()
+        new_cap = new_cap_str.split()
+        vocab = pickle.load(  # noqa: S301
+            Path.open("vocab/" + self.opt.dataset_mode + "_vocab.pkl", "rb"),
+        )
+        ori_txt = (
+            [vocab("<start>")]
+            + [vocab(word) for word in ori_cap]
+            + [vocab("<end>")]
+        )
+        ori_txt_tensor = torch.LongTensor(ori_txt).unsqueeze(0).cuda()
+
+        new_txt = (
+            [vocab("<start>")]
+            + [vocab(word) for word in new_cap]
+            + [vocab("<end>")]
+        )
+        new_txt_tensor = torch.LongTensor(new_txt).unsqueeze(0).cuda()
+        return ori_txt_tensor, new_txt_tensor
+
+    def edit_image_with_optimized_perturbations(
         self,
-        controlnet_type: ControlNetType = ControlNetType.CANNY,
-        device: torch.device | None = None,
-        num_inference_steps: int = 100,
-        image_guidance_scale: float = 1.0,
-        text_guidance_scale: float = 7.0,
-        seed: int = 0,
-        latent_guidance_scale: float = 25,
-    ) -> None:
-        self.model = load_controlnet(controlnet_type, device)
-        self.device = device
-        self.controlnet_type = controlnet_type
-        self.num_inference_steps = num_inference_steps
-        self.image_guidance_scale = image_guidance_scale
-        self.text_guidance_scale = text_guidance_scale
-        self.latent_guidance_scale = latent_guidance_scale
-        self.seed = seed
-
-    @property
-    def params_dict(self) -> dict[str, str | bool | int | float]:
-        return {
-            "device": str(self.device),
-            "controlnet_type": str(self.controlnet_type),
-            "num_inference_steps": self.num_inference_steps,
-            "image_guidance_scale": self.image_guidance_scale,
-            "text_guidance_scale": self.text_guidance_scale,
-            "latent_guidance_scale": self.latent_guidance_scale,
-            "seed": self.seed,
-        }
+        data: dict,
+        ori_tensor: torch.Tensor,
+        new_tensor: torch.Tensor,
+    ) -> Image.Image:
+        visuals = {}
+        for _ in range(self.optimize_iter):
+            self.open_edit_optimizer.run_opt_one_step(
+                data,
+                ori_tensor,
+                new_tensor,
+                self.alpha,
+                global_edit=self.global_edit,
+            )
+        visuals[
+            "optimized_manipulated"
+        ] = self.open_edit_optimizer.open_edit_model(
+            data,
+            mode="manipulate",
+            ori_cap=ori_tensor,
+            new_cap=new_tensor,
+            alpha=self.alpha,
+        )[0]
+        visuals = self.visualizer.convert_visuals_to_numpy(visuals, gray=True)
+        return Image.fromarray(visuals["optimized_manipulated"])
 
     def edit_image(
         self,
         prompt: str,
         image_path: str,
-        edit_info: Edit | None = None,
+        # edit_info: Edit | None = None,)
+        ori_cap: str,  # it would be the category
     ) -> Image.Image:
-
-    @property
-    def prompt_type(self) -> interfaces.ImageEditingPromptType:
-        return interfaces.ImageEditingPromptType.INSTRUCTION
-
-    def generate_prompt(self, edit: Edit) -> str:
-        return generate_instruction_based_prompt(edit)
+        input_image = self.image_loader(image_path)
+        ori_tensor, new_tensor = self.text_loader(ori_cap, prompt)
+        data = {"image": input_image, "caption": new_tensor, "length": [4]}
+        return self.edit_image_with_optimized_perturbations(
+            data,
+            ori_tensor,
+            new_tensor,
+        )
