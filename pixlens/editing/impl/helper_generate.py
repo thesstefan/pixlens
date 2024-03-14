a16dd8f238ff75f40e6922f2084dee594270a2ab
lluisp1999 <pastorlluis99@gmail.com>
2024-03-14 01:01:19 +0100

    Modified requirements, added helper generate

---
+++ pixlens/editing/impl/helper_generate.py
@@ -0,0 +1,210 @@
+import math
+import sys
+
+import numpy as np
+import torch
+from omegaconf import OmegaConf
+from PIL import Image, ImageFile
+from taming.models import cond_transformer, vqgan
+from torch import nn
+from torch.nn import functional as F
+
+sys.path.append("taming-transformers")
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+torch.backends.cudnn.benchmark = False
+
+
+def sinc(x):
+    return torch.where(
+        x != 0, torch.sin(math.pi * x) / (math.pi * x), x.new_ones([])
+    )
+
+
+def lanczos(x, a):
+    cond = torch.logical_and(-a < x, x < a)
+    out = torch.where(cond, sinc(x) * sinc(x / a), x.new_zeros([]))
+    return out / out.sum()
+
+
+def ramp(ratio, width):
+    n = math.ceil(width / ratio + 1)
+    out = torch.empty([n])
+    cur = 0
+    for i in range(out.shape[0]):
+        out[i] = cur
+        cur += ratio
+    return torch.cat([-out[1:].flip([0]), out])[1:-1]
+
+
+# For zoom video
+def zoom_at(img, x, y, zoom):
+    w, h = img.size
+    zoom2 = zoom * 2
+    img = img.crop((x - w / zoom2, y - h / zoom2, x + w / zoom2, y + h / zoom2))
+    return img.resize((w, h), Image.LANCZOS)
+
+
+# NR: Testing with different intital images
+def random_noise_image(w, h):
+    random_image = Image.fromarray(
+        np.random.randint(0, 255, (w, h, 3), dtype=np.dtype("uint8"))
+    )
+    return random_image
+
+
+# create initial gradient image
+def gradient_2d(start, stop, width, height, is_horizontal):
+    if is_horizontal:
+        return np.tile(np.linspace(start, stop, width), (height, 1))
+    else:
+        return np.tile(np.linspace(start, stop, height), (width, 1)).T
+
+
+def gradient_3d(width, height, start_list, stop_list, is_horizontal_list):
+    result = np.zeros((height, width, len(start_list)), dtype=float)
+
+    for i, (start, stop, is_horizontal) in enumerate(
+        zip(start_list, stop_list, is_horizontal_list)
+    ):
+        result[:, :, i] = gradient_2d(start, stop, width, height, is_horizontal)
+
+    return result
+
+
+def random_gradient_image(w, h):
+    array = gradient_3d(
+        w,
+        h,
+        (0, 0, np.random.randint(0, 255)),
+        (
+            np.random.randint(1, 255),
+            np.random.randint(2, 255),
+            np.random.randint(3, 128),
+        ),
+        (True, False, False),
+    )
+    random_image = Image.fromarray(np.uint8(array))
+    return random_image
+
+
+# Used in older MakeCutouts
+def resample(input, size, align_corners=True):
+    n, c, h, w = input.shape
+    dh, dw = size
+
+    input = input.view([n * c, 1, h, w])
+
+    if dh < h:
+        kernel_h = lanczos(ramp(dh / h, 2), 2).to(input.device, input.dtype)
+        pad_h = (kernel_h.shape[0] - 1) // 2
+        input = F.pad(input, (0, 0, pad_h, pad_h), "reflect")
+        input = F.conv2d(input, kernel_h[None, None, :, None])
+
+    if dw < w:
+        kernel_w = lanczos(ramp(dw / w, 2), 2).to(input.device, input.dtype)
+        pad_w = (kernel_w.shape[0] - 1) // 2
+        input = F.pad(input, (pad_w, pad_w, 0, 0), "reflect")
+        input = F.conv2d(input, kernel_w[None, None, None, :])
+
+    input = input.view([n, c, h, w])
+    return F.interpolate(
+        input, size, mode="bicubic", align_corners=align_corners
+    )
+
+
+class ReplaceGrad(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x_forward, x_backward):
+        ctx.shape = x_backward.shape
+        return x_forward
+
+    @staticmethod
+    def backward(ctx, grad_in):
+        return None, grad_in.sum_to_size(ctx.shape)
+
+
+replace_grad = ReplaceGrad.apply
+
+
+class ClampWithGrad(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, min, max):
+        ctx.min = min
+        ctx.max = max
+        ctx.save_for_backward(input)
+        return input.clamp(min, max)
+
+    @staticmethod
+    def backward(ctx, grad_in):
+        (input,) = ctx.saved_tensors
+        return (
+            grad_in * (grad_in * (input - input.clamp(ctx.min, ctx.max)) >= 0),
+            None,
+            None,
+        )
+
+
+def load_vqgan_model(config_path, checkpoint_path):
+    global gumbel
+    gumbel = False
+    config = OmegaConf.load(config_path)
+    if config.model.target == "taming.models.vqgan.VQModel":
+        model = vqgan.VQModel(**config.model.params)
+        model.eval().requires_grad_(False)
+        model.init_from_ckpt(checkpoint_path)
+    elif config.model.target == "taming.models.vqgan.GumbelVQ":
+        model = vqgan.GumbelVQ(**config.model.params)
+        model.eval().requires_grad_(False)
+        model.init_from_ckpt(checkpoint_path)
+        gumbel = True
+    elif (
+        config.model.target
+        == "taming.models.cond_transformer.Net2NetTransformer"
+    ):
+        parent_model = cond_transformer.Net2NetTransformer(
+            **config.model.params,
+        )
+        parent_model.eval().requires_grad_(False)
+        parent_model.init_from_ckpt(checkpoint_path)
+        model = parent_model.first_stage_model
+    else:
+        raise ValueError(f"unknown model type: {config.model.target}")
+    del model.loss
+    return model
+
+
+class Prompt(nn.Module):
+    def __init__(self, embed, weight=1.0, stop=float("-inf")):
+        super().__init__()
+        self.register_buffer("embed", embed)
+        self.register_buffer("weight", torch.as_tensor(weight))
+        self.register_buffer("stop", torch.as_tensor(stop))
+
+    def forward(self, input):
+        input_normed = F.normalize(input.unsqueeze(1), dim=2)
+        embed_normed = F.normalize(self.embed.unsqueeze(0), dim=2)
+        dists = (
+            input_normed.sub(embed_normed)
+            .norm(dim=2)
+            .div(2)
+            .arcsin()
+            .pow(2)
+            .mul(2)
+        )
+        dists = dists * self.weight.sign()
+        return (
+            self.weight.abs()
+            * replace_grad(dists, torch.maximum(dists, self.stop)).mean()
+        )
+
+
+def vector_quantize(x, codebook):
+    d = (
+        x.pow(2).sum(dim=-1, keepdim=True)
+        + codebook.pow(2).sum(dim=1)
+        - 2 * x @ codebook.T
+    )
+    indices = d.argmin(-1)
+    x_q = F.one_hot(indices, codebook.shape[0]).to(d.dtype) @ codebook
+    return replace_grad(x_q, x)
